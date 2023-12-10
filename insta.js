29a4b21f2ef2ae751dfa78328e608b74a081b701
anshikagupta-04 <pg8998211@gmail.com>
2023-12-10 18:48:26 +0530

    display done

---
+++ insta.js
@@ -0,0 +1,18 @@
+var photo = document.querySelector(".photo")
+var like = document.querySelector("#like")
+var like_btn = document.querySelector(".actions")
+photo.addEventListener("dblclick", function(){
+    like.style.transform = 'translate(-50%, -50%) scale(1)';
+    like.style.opacity = 0.9;
+    setTimeout(function(){
+        like_btn.innerHTML = '<i class="ri-heart-3-fill" id="like" style=" color: red; font-size: 30px;"></i><img src="comment.svg"><img src="share.svg">'
+    },300)
+
+    // setTimeout(function(){
+    //     like.style.opacity = 0;
+    // }, 1000)
+    
+    setTimeout(function(){
+        like.style.transform = 'translate(-50%, -50%) scale(0)'
+    },1000)
+})
